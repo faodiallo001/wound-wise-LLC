9ff64e33628d74125270bc8e8a5b2c33ced3d353
faodiallo001 <faodiallo001@gmail.com>
2026-07-03 16:29:58 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,3 +40,15 @@ tabs.forEach(tab => {
     });
 
 });
+
+const menuToggle =
+document.querySelector(".menu-toggle");
+
+const mobileMenu =
+document.querySelector(".mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+
+mobileMenu.classList.toggle("active");
+
+});
